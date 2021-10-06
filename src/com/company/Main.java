117282ac6c07c67fcb2e99a179bf254899dca73e
SkillfulDev <chernonog.evgeniy@gmail.com>
2021-10-06 21:00:01 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public class DataHolder {
+            private String data;
+
+            public DataHolder() {
+            }
+
+            public void setData(String data) {
+                this.data = data;
+            }
+
+            public String getData() {
+                return this.data;
+            }
+        }
+    }
+}
